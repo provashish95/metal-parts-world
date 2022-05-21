53bb4aab5111c2ff10ce2b01bd727266ea8efc9f
Provashish Roy <provashishroy95@gmail.com>
2022-05-22 02:07:50 +0600

    added navbar and banner section

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>navbar</h2>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark  navbar-style">
+            <div className="container">
+
+                <a class="navbar-brand" href="#">Meltal Parts World</a>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+                        <li className="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../images/banner/banner-1.jpg';
+import banner2 from '../../images/banner/banner-2.jpg';
+import banner3 from '../../images/banner/banner-3.jpg';
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
     return (
-        <div>
-            <h3>Banner</h3>
-        </div>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>We give best services</h3>
+                    <p>Our company is our first priority and they are our luck</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Trust is our wealth</h3>
+                    <p>We stay always people with good service</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Best warehouse of world</h3>
+                    <p>
+                        Book is good friend of mind
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
