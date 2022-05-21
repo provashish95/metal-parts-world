ac8c72715a0b71f8fcbfe682f870b752c36091a0
Provashish Roy <provashishroy95@gmail.com>
2022-05-22 03:17:33 +0600

    make carosel responsive

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -3,6 +3,7 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../images/banner/banner-1.jpg';
 import banner2 from '../../images/banner/banner-2.jpg';
 import banner3 from '../../images/banner/banner-3.jpg';
+import './Banner.css';
 
 const Banner = () => {
     const [index, setIndex] = useState(0);
@@ -32,8 +33,8 @@ const Banner = () => {
                 />
 
                 <Carousel.Caption>
-                    <h3>Trust is our wealth</h3>
-                    <p>We stay always people with good service</p>
+                    <h3>We give best services</h3>
+                    <p>Our company is our first priority and they are our luck</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -44,10 +45,8 @@ const Banner = () => {
                 />
 
                 <Carousel.Caption>
-                    <h3>Best warehouse of world</h3>
-                    <p>
-                        Book is good friend of mind
-                    </p>
+                    <h3>We give best services</h3>
+                    <p>Our company is our first priority and they are our luck</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
