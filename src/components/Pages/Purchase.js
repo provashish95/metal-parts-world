9d262b1a6a984affb59fdfb01d25325d8f4b96e0
Provashish Roy <provashishroy95@gmail.com>
2022-05-24 00:46:04 +0600

    purchase page complete

--- src/components/Pages/Purchase.js
+++ src/components/Pages/Purchase.js
@@ -4,13 +4,12 @@ import { useParams } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Loading from '../shared/Loading';
 import { toast } from 'react-toastify';
+import { set } from 'react-hook-form';
 
 const Purchase = () => {
     const [user] = useAuthState(auth);
     const { id } = useParams();
-    const [orderQuantity, setOrderQuantity] = useState('');
-    const [isReload, setIsReload] = useState(true);
-    const [isDisable, setIsDisable] = useState(true);
+    const [isDisable, setIsDisable] = useState(false);
     const [product, setProduct] = useState({});
     const { name, img, description, price, minimumOrderQuantity, quantity } = product;
     const nameRef = useRef("");
@@ -19,20 +18,21 @@ const Purchase = () => {
     const addressRef = useRef("");
     const quantityRef = useRef("");
 
+
+
     useEffect(() => {
         fetch(`http://localhost:5000/products/${id}`)
             .then(res => res.json())
             .then(data => {
                 setProduct(data);
             });
-    }, [isReload, id])
-
+    }, [id])
 
     if (Object.keys(product).length === 0) {
         return <Loading></Loading>
     }
 
-    const handleQuantity = event => {
+    const handleOrder = (event) => {
         event.preventDefault();
         const UserQuantity = quantityRef.current.value;
 
@@ -40,40 +40,13 @@ const Purchase = () => {
         const minQuantity = parseInt(minimumOrderQuantity);
         const availQuantity = parseInt(quantity);
 
-
-        if (myQuantity < minQuantity) {
-            toast.error('Your quantity lower than minimum quantity')
-            event.target.reset();
-            setIsDisable(true);
-            return;
-        } else if (myQuantity > availQuantity) {
-            toast.error('Your quantity higher than available quantity')
-            event.target.reset();
-            setIsDisable(true);
-            return;
-        } else if (isNaN(myQuantity)) {
-            toast.error('Please Enter Valid number')
-            event.target.reset();
-            setIsDisable(true);
-            return;
-        } else {
-            setOrderQuantity(myQuantity);
-            setIsDisable(false);
-            toast.success('Added Your quantity')
-            event.target.reset();
-        }
-    }
-
-    const handleOrder = (event) => {
-        event.preventDefault();
         const name = nameRef.current.value;
         const email = emailRef.current.value;
         const phone = phoneRef.current.value;
         const address = addressRef.current.value;
         const newPrice = parseInt(price);
-        const totalPrice = newPrice * orderQuantity;
-        const oldQuantity = parseInt(quantity);
-        const updateQuantity = oldQuantity - orderQuantity
+        const totalPrice = newPrice * myQuantity;
+
         const orders = {
             productId: id,
             userName: name,
@@ -81,43 +54,48 @@ const Purchase = () => {
             userPhone: phone,
             address: address,
             price: totalPrice,
-            orderQuantity: orderQuantity
+            orderQuantity: myQuantity
         }
-        //console.log(updateQuantity);
-
-        //update product quantity 
-        fetch(`http://localhost:5000/updateQuantity/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ updateQuantity }),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                setIsReload(!isReload);
-            });
-
-        //Booking order
-        fetch('http://localhost:5000/orders', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(orders)
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                toast.success(data.success)
-                event.target.reset()
-            });
+        //check quantity here 
+        if (myQuantity < minQuantity) {
+            toast.error(`Your quantity lower than ${minQuantity}`);
+            setIsDisable(true);
+            return;
+        } else if (myQuantity > availQuantity) {
+            toast.error(`Your quantity higher than ${availQuantity}`);
+            setIsDisable(true);
+            return;
+        } else if (isNaN(myQuantity)) {
+            toast.error('Please Enter Valid number');
+            setIsDisable(true);
+            return;
+        } else {
+            //Booking order here
+            fetch('http://localhost:5000/orders', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(orders)
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    toast.success(data.success)
+                    event.target.reset()
+                });
+        }
     }
+
+    const onChnageQuantity = event => {
+        setIsDisable(false);
+    }
+
 
     return (
         <div className="container my-5">
             <h5 className='text-center my-5'>Purchase Your Product</h5>
             <div className="row g-0 justify-content-center align-items-center">
-                <div className="col-sm-12 col-md-8 col-lg-8">
+                <div className="col-12">
                     <div className="card mb-3" >
                         <div className="row g-0 justify-content-center align-items-center p-3 ">
                             <div className="col-md-4 text-center py-5">
@@ -135,36 +113,36 @@ const Purchase = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col-sm-12 col-md-4 col-lg-4 ">
-                    <div className='m-4'>
-                        <form onSubmit={handleQuantity} className='w-100 mx-auto text-center'  >
-                            <div className="mb-3">
-                                <input type="text" ref={quantityRef} className="w-100 mx-auto rounded input-style p-1" id="exampleInputName" placeholder='Add Your Quantity' required />
-                            </div>
-                            <button type="submit" className='btn  secondary-color w-50 mx-auto d-block mb-5'>Add Quantity</button>
-                        </form>
-                    </div>
-                </div>
+
             </div>
             <div className="row my-5">
                 <div className="col-12">
                     <div className='w-50 mx-auto'>
-                        <h5 className='text-center mb-5'>Customer info</h5>
+                        <p className='text-center fw-bold'>Minimum Quantity</p>
                         <form onSubmit={handleOrder}>
+                            <div className="mb-3 text-center">
+                                <input type="text" value={minimumOrderQuantity} className="w-50 mx-auto rounded text-center  p-1" readOnly disabled />
+                            </div>
+                            <p className='text-center fw-bold'>Add your quantity</p>
+                            <div className="mb-3 text-center">
+                                <input type="text" onChange={onChnageQuantity} ref={quantityRef} className="w-50 mx-auto rounded text-center  p-1" required />
+                            </div>
+                            <p className='text-center fw-bold'>Add your Info</p>
                             <div className="mb-3">
-                                <input type="text" ref={nameRef} name="name" value={user?.displayName} className="w-100 rounded  p-1" id="exampleInputName" placeholder='Your Name' disabled />
+                                <input type="text" ref={nameRef} name="name" value={user?.displayName} className="w-100 rounded  p-1" readOnly disabled />
                             </div>
                             <div className="mb-3">
-                                <input type="email" ref={emailRef} name="email" value={user?.email} className="w-100 rounded p-1" id="exampleInputEmail" placeholder='Your Email' disabled />
+                                <input type="email" ref={emailRef} name="email" value={user?.email} className="w-100 rounded p-1" readOnly disabled />
                             </div>
                             <div className="mb-3">
-                                <input type="text" ref={phoneRef} name="phone" className="w-100 rounded  p-1" id="exampleInputPhone" placeholder='Your Phone' required />
+                                <input type="text" ref={phoneRef} name="phone" className="w-100 rounded  p-1" placeholder='Your Phone' required />
                             </div>
                             <div className="mb-3">
-                                <textarea ref={addressRef} className="w-100 rounded" placeholder="Add Your Address" id="floatingTextarea2" required></textarea>
+                                <textarea ref={addressRef} className="w-100 rounded" placeholder="Add Your Address" required></textarea>
                             </div>
                             <button type="submit" className='btn btn-primary w-50 mx-auto d-block mb-5' disabled={isDisable}>Order Now</button>
                         </form>
+
                     </div>
                 </div>
             </div>
