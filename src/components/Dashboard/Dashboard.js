f1ba19e40506545d8d3a3e7334e390957604669d
Provashish Roy <provashishroy95@gmail.com>
2022-05-25 01:08:28 +0600

    make side nabar and set nested route

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+
 
 const Dashboard = () => {
+
+
     return (
-        <div>
-            <h5>Dashboard</h5>
-            <Outlet></Outlet>
+        <div className='container-fluid'>
+            <div className="row">
+                <div className="col-sm-12 col-md-2 col-lg-2 border p-0 m-0">
+                    <ul className='side-nav-ul border shadow-lg'>
+                        <li className='side-nav-link border p'><Link to='/dashboard'>My Profile</Link></li>
+                        <li className='side-nav-link border'><Link to='/dashboard/MyOrders'>My Orders</Link></li>
+                        <li className='side-nav-link border'><Link to='/dashboard/review'>Add A Review</Link></li>
+                        <li className='side-nav-link border'><Link to='/dashboard'>My Profile</Link></li>
+                        {/* for admin .... */}
+                        <li className='side-nav-link border'><Link to='/dashboard'>Manage All Orders</Link></li>
+                        <li className='side-nav-link border'><Link to='/dashboard'>Add A Product</Link></li>
+                        <li className='side-nav-link border'><Link to='/dashboard'>Manage Products</Link></li>
+                        <li className='side-nav-link border'><Link to='/dashboard'>Make Admin</Link></li>
+                    </ul>
+                </div>
+                <div className="col-sm-12 col-md-10 col-lg-10">
+                    <h2>Dashboard</h2>
+                    <Outlet></Outlet>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Dashboard;
+
--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
                         <li className="nav-item">
                             <Link className="nav-link" to="/home">Home</Link>
@@ -35,8 +35,9 @@ const Navbar = () => {
                         </li>
                         <li className="nav-item">
                             {
-                                user ? <Link className="nav-link" to="/dashboard">Dashboard</Link> : " "
+                                user ? <Link className="nav-link" to="/dashboard"  >Dashboard</Link> : " "
                             }
+
                         </li>
                     </ul>
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
