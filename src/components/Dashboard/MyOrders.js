fd032f80bd62f3363815d39d0fced8f35ed44522
Provashish Roy <provashishroy95@gmail.com>
2022-05-25 03:55:12 +0600

    load all my orders by email id

--- src/components/Dashboard/MyOrders.js
+++ src/components/Dashboard/MyOrders.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../shared/Loading';
 
 const MyOrders = () => {
+    const [myOrders, setMyOrders] = useState();
+    const [user, loading] = useAuthState(auth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/orders?userEmail=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/')
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setMyOrders(data)
+                })
+        }
+    }, [user, navigate]);
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h2>My odrers</h2>
+            <h2>My odrers : {myOrders?.length}</h2>
         </div>
     );
 };
