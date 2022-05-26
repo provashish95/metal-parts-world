c911e29ad753e68ee67bbba841e585a82f5e7918
Provashish Roy <provashishroy95@gmail.com>
2022-05-26 11:46:11 +0600

    completed my profile

--- src/components/Dashboard/MyProfile.js
+++ src/components/Dashboard/MyProfile.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 
 import auth from '../../firebase.init';
 import Loading from '../shared/Loading';
 
 const MyProfile = () => {
     const [user, loading] = useAuthState(auth);
-    const [phone, setPhone] = useState('');
-
 
     const { data: userProfile, isLoading, refetch } = useQuery('userProfile', () => fetch(`http://localhost:5000/users/${user.email}`, {
         headers: {
@@ -23,71 +21,110 @@ const MyProfile = () => {
 
     const handleUpdate = (event) => {
         event.preventDefault();
-        //const phone = event.target.phone.value;
-        console.log(phone);
-        const currentUser = { phone: phone }
+
+        const phone = event.target.phone.value;
+        const address = event.target.address.value;
+        const linkedin = event.target.linkedin.value;
+        const nid = event.target.nid.value;
+        const education = event.target.education.value;
+
+        const userInfo = {
+            phone: phone,
+            address: address,
+            linkedin: linkedin,
+            nid: nid,
+            education: education
+        }
+
         fetch(`http://localhost:5000/users/${user.email}`, {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(currentUser)
+            body: JSON.stringify(userInfo)
         })
             .then(res => res.json())
             .then(data => {
                 refetch()
-                console.log(data);
+                toast.success('updated your profile');
+                event.target.reset();
             })
 
-
     }
 
-
-
-
-    const onChangePhone = e => {
-        const phone = e.target.value;
-        setPhone(phone);
-        refetch()
-    }
 
     return (
         <div className='container'>
-            <div className="row w-50 mx-auto">
-                <div className="col-12">
+            <div className="row ">
+                <div className="col-sm-12 col-md-6 col-lg-6">
+
+                    <p className='text-center fw-bold'>Your Profile nfo</p>
+                    <label className="form-label">Your Name</label>
+                    <div className="mb-3">
+                        <input type="text" name="name" value={user?.displayName} className="w-100 rounded  p-1" readOnly disabled />
+                    </div>
+                    <label className="form-label">Your Email</label>
+                    <div className="mb-3">
+                        <input type="email" name="email" value={user?.email} className="w-100  p-1" readOnly disabled />
+                    </div>
+                    <label className="form-label">Phone</label>
+                    <div className="mb-3">
+                        <input type="text" name="phone" value={userProfile.phone} className="w-100   p-1" readOnly disabled />
+                    </div>
+                    <label className="form-label"> Location (city/district)</label>
+                    <div className="mb-3">
+                        <textarea value={userProfile?.address} className="w-100 " readOnly disabled ></textarea>
+                    </div>
+                    <label className="form-label">Education</label>
+                    <div className="mb-3">
+                        <textarea value={userProfile?.education} className="w-100 " readOnly disabled ></textarea>
+                    </div>
+                    <label className="form-label">Linkedin Profile url</label>
+                    <div className="mb-3">
+                        <input type="text" name="linkedin" value={userProfile?.linkedin} className="w-100   p-1" readOnly disabled />
+                    </div>
+                    <label className="form-label">NID</label>
+                    <div className="mb-3">
+                        <input type="text" name="nid" value={userProfile?.nid} className="w-100   p-1" readOnly disabled />
+                    </div>
+                    <label className="form-label">Role</label>
+                    <div className="mb-3">
+                        <input type="text" name="role" value={userProfile?.role} className="w-100   p-1" readOnly disabled />
+                    </div>
+
+                </div>
+                <div className='col-sm-12 col-md-6 col-lg-6'>
                     <form onSubmit={handleUpdate}>
-                        <p className='text-center fw-bold'>Your Profile</p>
+                        <p className='text-center fw-bold'>Update Your Profile</p>
                         <label className="form-label">Your Name</label>
                         <div className="mb-3">
                             <input type="text" name="name" value={user?.displayName} className="w-100 rounded  p-1" readOnly disabled />
                         </div>
                         <label className="form-label">Your Email</label>
                         <div className="mb-3">
-                            <input type="email" name="email" value={user?.email} className="w-100  p-1" readOnly disabled />
+                            <input type="email" value={user?.email} className="w-100  p-1" readOnly disabled />
                         </div>
                         <label className="form-label">Phone</label>
                         <div className="mb-3">
-                            <input type="text" onChange={onChangePhone} name="phone" value={userProfile.phone} className="w-100   p-1" required />
+                            <input type="text" name="phone" className="w-100   p-1" required />
                         </div>
                         <label className="form-label"> Location (city/district)</label>
                         <div className="mb-3">
-                            <textarea value={userProfile?.address} className="w-100 " required></textarea>
+                            <textarea name="address" className="w-100 " required></textarea>
                         </div>
                         <label className="form-label">Education</label>
                         <div className="mb-3">
-                            <textarea value={userProfile?.education} className="w-100 " required></textarea>
+                            <textarea className="w-100" name="education" required></textarea>
                         </div>
                         <label className="form-label">Linkedin Profile url</label>
                         <div className="mb-3">
-                            <input type="text" name="linkedin" value={userProfile?.linkedin} className="w-100   p-1" required />
+                            <input type="text" name="linkedin" className="w-100   p-1" required />
                         </div>
                         <label className="form-label">NID</label>
                         <div className="mb-3">
-                            <input type="text" name="nid" value={userProfile?.linkedin} className="w-100   p-1" required />
+                            <input type="text" name="nid" className="w-100   p-1" required />
                         </div>
-
-
-                        <button type="submit" className='btn btn-dark w-50 mx-auto d-block mb-5'>Update</button>
+                        <button type="submit" className='btn btn-dark w-50 mx-auto d-block mb-5' >Update</button>
                     </form>
                 </div>
             </div>
